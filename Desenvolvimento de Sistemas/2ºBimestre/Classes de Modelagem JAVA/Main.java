d0e1448888fdee71c686645cf6398d8b9bf1c6b5
Gustavo <gustavoale1759@gmail.com>
2025-06-03 11:26:52 -0300

    Delete Desenvolvimento de Sistemas/2ºBimestre/Classes de Modelagem JAVA directory

--- Desenvolvimento de Sistemas/2ºBimestre/Classes de Modelagem JAVA/Main.java
+++
@@ -1,8 +0,0 @@
-package pessoa;
-public class Main {
-     public static void main(String[] args) {
-        Pessoa pessoa = new Pessoa();
-        pessoa.inserirDadosPessoa();
-        pessoa.apresentarPessoa();
-    }
-}
--- Desenvolvimento de Sistemas/2ºBimestre/Classes de Modelagem JAVA/Principal.java
+++
@@ -1,9 +0,0 @@
-public class Principal {
-    public static void main(String[] args) {
-
-        Usuario usuario1 = new Usuario();
-
-        usuario1.provarExistencia();
-    }
-}
-
