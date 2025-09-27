24b8ad1bbbd1696a627a45d9e8945b85f02f3248
Gustavo Alexandre da Silva <gustavoale1759@gmail.com>
2025-09-27 14:54:32 -0300

    Delete Programação Web II directory

--- Programação Web II/1ºBimestre/PHP e HTML/par_impar.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $numero = $_POST['numero'];
-    
-    if ($numero % 2 == 0) {
-        echo "O número " . $numero . " é Par.";
-    } else {
-        echo "O número " . $numero . " é Ímpar.";
-    }
-?>
--- Programação Web II/1ºBimestre/PHP e HTML/tabuada.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $numero = $_POST['numero'];
-    echo "<h1>Tabuada do " . $numero . "</h1>";
-    for ($i = 1; $i <= 10; $i++) {
-        echo $numero . " x " . $i . " = " . ($numero * $i) . "<br>";
-    }
-?>
